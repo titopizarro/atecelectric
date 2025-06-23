3b42c69c46fac1760ea718b729bd3fd3b48806e6
Tito Pizarro <titomauriciopizarroochoa@gmail.com>
2025-06-22 20:52:12 -0500

    Configure Next.js for static export with base path and asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Habilita la exportación estática
+  basePath: "/atecelectric",
+  assetPrefix: "./",
+};
 
 export default nextConfig;
